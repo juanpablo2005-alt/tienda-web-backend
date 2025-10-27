1c16146c283128c02e6a5a5732ed23c74f1ad0ca
PABLO03 <123429802+juanpablo2005-alt@users.noreply.github.com>
2025-10-27 05:58:57 -0500

    Delete backend - Copy directory

--- backend - Copy/models/Product.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const productSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    price: { type: Number, required: true },
-    available: { type: Boolean, default: true }
-});
-
-export const Product = mongoose.model("Product", productSchema);
--- backend - Copy/routes/product.js
+++
@@ -1,6 +0,0 @@
-import express from "express";
-import { getAvailableProducts } from "../controllers/productController.js";
-
-const router = express.Router();
-router.get("/", getAvailableProducts);
-export default router;
